a6b79ab231b044b025f6db0f54abb8d20cd671ce
Gummibjorn <marioegli95@gmail.com>
2015-11-16 16:53:28 +0100

    Started implementing BusinessLayer

--- AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
+++ AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
@@ -1,14 +1,34 @@
-﻿namespace AutoReservation.BusinessLayer
+﻿using AutoReservation.Dal;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AutoReservation.BusinessLayer
 {
     public class AutoReservationBusinessComponent
     {
 
-        //private static void HandleDbConcurrencyException<T>(AutoReservationEntities context, T original) where T : class
-        //{
-        //    var databaseValue = context.Entry(original).GetDatabaseValues();
-        //    context.Entry(original).CurrentValues.SetValues(databaseValue);
+        private AutoReservationEntities context;
 
-        //    throw new LocalOptimisticConcurrencyException<T>(string.Format("Update {0}: Concurrency-Fehler", typeof(T).Name), original);
-        //}
+        public AutoReservationBusinessComponent(){
+                    context = new AutoReservationEntities();
+        }
+
+        public List<Auto> allAutos()
+        {
+            return context.Autos.AsNoTracking().ToList();                
+        }
+
+        public Auto getAuto(int id)
+        {
+            return context.Autos.AsNoTracking().SingleOrDefault(i => i.Id == id);
+        }
+
+        private static void HandleDbConcurrencyException<T>(AutoReservationEntities context, T original) where T : class
+        {
+            var databaseValue = context.Entry(original).GetDatabaseValues();
+            context.Entry(original).CurrentValues.SetValues(databaseValue);
+
+            throw new LocalOptimisticConcurrencyException<T>(string.Format("Update {0}: Concurrency-Fehler", typeof(T).Name), original);
+        }
     }
 }
--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,7 +1,25 @@
-﻿namespace AutoReservation.Common.Interfaces
+﻿using AutoReservation.Common.DataTransferObjects;
+using System.Collections.Generic;
+
+namespace AutoReservation.Common.Interfaces
 {
     public interface IAutoReservationService
     {
+        List<AutoDto> allAutos();
+        AutoDto getAuto(int id);
+        AutoDto insertAuto(AutoDto newAuto);
+        AutoDto updateAuto(AutoDto modified, AutoDto original);
+
+        List<ReservationDto> allReservations();
+        ReservationDto getReservation(int id);
+        ReservationDto insertReservation(ReservationDto newReservation);
+        ReservationDto updateReservation(ReservationDto modified, ReservationDto original);
+
+         List<ReservationDto> allKunde();
+        KundeDto getKunde(int id);
+        KundeDto insertKunde(KundeDto newReservation);
+        KundeDto updateKunde(KundeDto modified, KundeDto original);
         
+
     }
 }
