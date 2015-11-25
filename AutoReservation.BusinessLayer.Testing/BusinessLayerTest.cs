843b70bf8ef17055775c9891b8355e5bc68a18f2
fschlaepfer <backslash7@gmail.com>
2015-11-25 10:10:10 +0000

    Add missing files for previous commit

--- AutoReservation.BusinessLayer.Testing/BusinessLayerTest.cs
+++ AutoReservation.BusinessLayer.Testing/BusinessLayerTest.cs
@@ -29,17 +29,49 @@ namespace AutoReservation.BusinessLayer.Testing
         [TestMethod]
         public void Test_UpdateAuto()
         {
-            
+            const string marke = "Test Marke";
+            const int id = 1;
+
+            var originalAuto = Target.GetAuto(id);
+            var modifiedAuto = Target.GetAuto(id);
+
+            modifiedAuto.Marke = marke;
+            Target.UpdateAuto(modifiedAuto, originalAuto);
+
+            var modifiedObjectInList = Target.GetAuto(id);
+            Assert.AreEqual(marke, modifiedObjectInList.Marke);
         }
 
         [TestMethod]
         public void Test_UpdateKunde()
         {
+            const string nachname = "Test Nachname";
+            const int id = 1;
+
+            var originalKunde = Target.GetKunde(id);
+            var modifiedKunde = Target.GetKunde(id);
+
+            modifiedKunde.Nachname = nachname;
+            Target.UpdateKunden(modifiedKunde, originalKunde);
+
+            var modifiedObjectInList = Target.GetKunde(id);
+            Assert.AreEqual(nachname, modifiedObjectInList.Nachname);
         }
 
         [TestMethod]
         public void Test_UpdateReservation()
         {
+            System.DateTime bis = System.DateTime.Parse("1.2.3456 12:34");
+            const int id = 1;
+
+            var originalReservation = Target.GetReservation(id);
+            var modifiedReservation = Target.GetReservation(id);
+
+            modifiedReservation.Bis = bis;
+            Target.UpdateReservation(modifiedReservation, originalReservation);
+
+            var modifiedObjectInList = Target.GetReservation(id);
+            Assert.AreEqual(bis, modifiedObjectInList.Bis);
         }
     }
 }
--- AutoReservation.Dal/AutoReservationDataModel.Designer.cs
+++ AutoReservation.Dal/AutoReservationDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Mario\Documents\MsTE\Miniprojekt\Vorgabe\AutoReservation.Dal\AutoReservationDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\fabian\Documents\GitHubVisualStudio\MsTeMiniprojekt\AutoReservation.Dal\AutoReservationDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
