fd2113ea1195ae8b3a248771e7d081f88dc86461
Gummibjorn <marioegli95@gmail.com>
2015-11-30 14:19:52 +0100

    Implement KundenViewModel (copy from AutoViewModel) and RemoteDataAccessFactory

---
+++ AutoReservation.Ui/Factory/RemoteDataAccessServiceFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoReservation.Common.Interfaces;
+using System.ServiceModel;
+
+namespace AutoReservation.Ui.Factory
+{
+    class RemoteDataAccessServiceFactory : IServiceFactory
+    {
+        private IAutoReservationService target;
+
+        public IAutoReservationService GetService()
+        {
+            if (target == null)
+            {
+                ChannelFactory<IAutoReservationService> channelFactory = new ChannelFactory<IAutoReservationService>("AutoReservationService");
+                target = channelFactory.CreateChannel();
+            }
+            return target;
+        }
+    }
+}
